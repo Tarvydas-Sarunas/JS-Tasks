684324c024411c0f74b6295d5a110496b8b86191
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-08 14:09:21 +0100

    planas pirmam taskui

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,17 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 div elemente su id "output" viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+// 1. nusitaikau i form ir input
+const form = document.getElementById("form");
+// 2. padarau input submit add.eventListiner
+// 3. paspaudus jis nukreipia i functija skaiciavimo funkcija
+// 4. kad suskaiciuotu patikrinu ar skaicius !== Nan
+// 5. tokiu atvieju jis skaiciuoja
+// 6. kad skaiciuotu kuriu kintamuosius kiekvienam paskaiciavimui
+// 6a. skaiciuoja svarus
+// 6b. skaiciuoja gramus
+// 6c. skaiciuoja uncijas
+// 7. kuriu ul ir li i kuriuos talpinu textContent paskaiciavimus
+// 8. kitu atveju ismeta h2 privalote ivesti skaicius
+// kuri taip pat kuriu
