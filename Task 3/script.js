f7df1006a54aec887e9f043962368df7b3f40eba
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-09 11:21:55 +0100

    task 3 primituvus planas

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,14 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+// 1. nusitaikau i btn, output, message
+// 2. isitraukiam su fetch ir then endpoint informacija
+// 3.antrame then iskvieciu funkcija kuri suks cikla forEach
+// 4. suku cikla ir darau du kintamuosius kuriuas pateikiu kaip argumenta i funkcija creatCard()
+// 5. duodu btn .addListener kuris po click daro dvi funkcijas
+// 5a. kuria card ir paslepiu message p tag
+
+// 6. kuriu funkcija createACard
+// 7. Kuriu funkcija hideTheMessage
