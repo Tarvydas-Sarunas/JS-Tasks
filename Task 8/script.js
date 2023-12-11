a8d4ae3e7230a3efb0bdb93233dc998c930371f3
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-11 08:51:05 +0100

    astuntas veikia

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,24 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+class Calculator {
+  sum(a, b) {
+    return a + b;
+  }
+
+  subtraction(a, b) {
+    return a - b;
+  }
+  multiplication(a, b) {
+    return a * b;
+  }
+  division(a, b) {
+    return a / b;
+  }
+}
+
+const calculator = new Calculator();
+const sudetis = calculator.sum(6, 7);
+const atimtis = calculator.subtraction(15, 3);
+const daugyba = calculator.multiplication(2, 9);
+const dalyba = calculator.division(21, 7);
